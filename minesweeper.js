523449f1071047eade3f58d61ec1735066fa9636
iamchenjiajun <iamchenjiajun@gmail.com>
2020-09-12 11:24:48 +0800

    Add new game button

--- minesweeper.js
+++ minesweeper.js
@@ -2,4 +2,8 @@ import Board from './board.js';
 
 window.addEventListener("load", () => {
     let board = new Board(16, 30);
+
+    document.getElementById("minesweeper-newgame").onclick = () => {
+        board = new Board(16, 30);
+    }
 });
