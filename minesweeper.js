f7d3225132e8d75620dc2f48b8cf91e1c45bd243
iamchenjiajun <iamchenjiajun@gmail.com>
2020-09-14 16:28:51 +0800

    Add 3 difficulty levels

--- minesweeper.js
+++ minesweeper.js
@@ -3,7 +3,13 @@ import Board from './board.js';
 window.addEventListener("load", () => {
     let board = new Board(16, 30, 99);
 
-    document.getElementById("minesweeper-newgame").onclick = () => {
+    document.getElementById("minesweeper-difficulty-1").onclick = () => {
+        board = new Board(9, 9, 10);
+    }
+    document.getElementById("minesweeper-difficulty-2").onclick = () => {
+        board = new Board(16, 16, 40);
+    }
+    document.getElementById("minesweeper-difficulty-3").onclick = () => {
         board = new Board(16, 30, 99);
     }
 });
